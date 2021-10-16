e3996f2cae47924c08d30fc3a0b15f801c64483d
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2021-10-16 12:18:34 +0530

    add port

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server Running on PORT: ${PORT}`)
