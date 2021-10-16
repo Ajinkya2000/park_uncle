c79e035d36b76503caeab4a3630313eaaa4b01b0
Sushant <skp000002@gmail.com>
2021-10-16 21:20:15 +0530

    Add marker to user location

--- client/src/components/Map/BasicMap.js
+++ client/src/components/Map/BasicMap.js
@@ -1,40 +1,52 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
 
 import useGeoLocation from "../../hooks/useGeoLocation";
 
 // Styles
 import styles from "./Map.module.css";
 
+// Action import
+import { setUserMarker } from "../../store/actions";
+
 // Config
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-const BasicMap = ({mapState}) => {
+const BasicMap = ({ mapState, setUserMarker }) => {
+  const [helper, setHelper] = useState(true);
 
   const mapContainer = useRef(null);
   const map = useRef(null);
   const location = useGeoLocation();
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v11",
       center: [mapState.currentLongitude, mapState.currentLatitude],
       zoom: mapState.zoom,
     });
-  });  
 
-  // useEffect(() => {
-  //   // location.loaded ? console.log(JSON.stringify(location)) : console.log("Please allow location!!")
+    new mapboxgl.Marker({
+      color: "#000",
+      draggable: false,
+    })
+      .setLngLat([mapState.currentLongitude, mapState.currentLatitude])
+      .addTo(map.current);
 
-  //   if(location.loaded && !location.error) {
-  //     setLng(location.coordinates.lng);
-  //     setLat(location.coordinates.lat);
-  //   }
+    // eslint-disable-next-line
+  }, [helper]);
 
-  // }, [location]);
+  useEffect(() => {
+    if (location.loaded && !location.error) {
+      setUserMarker(location.coordinates.lng, location.coordinates.lat);
+      setHelper(!helper);
+    }
+
+    // eslint-disable-next-line
+  }, [location]);
 
   // useEffect(() => {
   //   if (!map.current) return; // wait for map to initialize
@@ -44,6 +56,7 @@ const BasicMap = ({mapState}) => {
   //     setZoom(map.current.getZoom().toFixed(2));
   //   });
   // });
+
   return (
     <div className={styles.mapWrapper}>
       <div ref={mapContainer} className={styles.mapContainer} />
@@ -53,8 +66,8 @@ const BasicMap = ({mapState}) => {
 
 const mapStateToProps = (state) => {
   return {
-    mapState: state.mapReducer
-  }
-}
+    mapState: state.mapReducer,
+  };
+};
 
-export default connect(mapStateToProps)(BasicMap);
+export default connect(mapStateToProps, { setUserMarker })(BasicMap);
